a6481fef1246f6a1ec08b2810a66c44362b803c9
sur3shbabu <66028090+sur3shbabu@users.noreply.github.com>
2020-05-31 20:37:18 +0530

    day1 workout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
